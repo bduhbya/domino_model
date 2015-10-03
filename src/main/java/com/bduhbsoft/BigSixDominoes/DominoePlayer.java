a346093bd32d2670e56adc09868c65aedd6e0d05
brweathe <brweathe@microsoft.com>
2015-10-02 20:52:30 -0700

    Added Javadoc to player object

--- src/main/java/com/bduhbsoft/BigSixDominoes/DominoePlayer.java
+++ src/main/java/com/bduhbsoft/BigSixDominoes/DominoePlayer.java
@@ -19,26 +19,62 @@ public class DominoePlayer {
         mScore = 0;
     }
 
+    /**
+    * Returns the display name of the player
+    *
+    * @return String : the display name
+    */
     public String getDisplayName() {
         return mDisplayName;
     }
 
+    /**
+    * Returns the user name of the player.
+    * <p/>
+    * User name is guaranteed unique
+    *
+    * @return String : the user name
+    */
     public String getUserName() {
         return mUserName;
     }
 
+    /**
+    * Returns the current player's turn status
+    *
+    * @return boolean User's turn status
+    */
     public boolean isMyTurn() {
         return mIsMyTurn;
     }
 
+    /**
+    * Returns the current player's score
+    *
+    * @return boolean User's turn status
+    */
     public int getScore() {
         return mScore;
     }
 
+    /**
+    * Set's the current player's turn status
+    * <p/>
+    * Is not checked.  Containing structure repsonsible for managing
+    *
+    * @param boolean : User's turn status
+    */
     public void setMyTurn(boolean myTurn) {
         mIsMyTurn = myTurn;
     }
 
+    /**
+    * Set's the current player's score
+    * <p/>
+    * Is not checked.  Containing structure repsonsible for managing
+    *
+    * @param int : User's score
+    */
     public void setScore(int newScore) {
         mScore = newScore;
     }
