f2f7cf7a3ed0b8ab18cbad8493995b630d19445d
brweathe <brweathe@microsoft.com>
2018-03-30 14:47:30 -0700

    Updated build to grab junit deps and added them to classpath

--- build-me.sh
+++ build-me.sh
@@ -8,7 +8,8 @@ usage(){
 
 if [ $# -eq 0 ]
 then
-    javac -Xlint:unchecked src/main/java/com/bduhbsoft/BigSixDominoes/*.java
+    ./get-deps.sh
+    javac -cp .:junit-4.12.jar:hamcrest-core-1.3.jar -Xlint:unchecked src/main/java/com/bduhbsoft/BigSixDominoes/*.java
     #jar cvf BigSixDominoes.jar src/main/java/com/bduhbsoft/BigSixDominoes/*.class
     #jar cvf src/main/java/com/bduhbsoft/BigSixDominoes/BigSixDominoes.jar src/java/com/bduhbsoft/BigSixDominoes/*.class src/java/com/bduhbsoft/BigSixDominoes/*.java
     #javac -classpath BigSixDominoes.jar:. Testing/src/com/bduhbsoft/funcTesting/*.java
---
+++ get-deps.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -O junit-4.12.jar
+wget http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -O hamcrest-core-1.3.jar
